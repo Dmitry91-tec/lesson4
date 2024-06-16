6cb8f271c413a18bd3c4fe28700ec570d91787e0
Dmitry91-tec <nasdi91@yandex.ru>
2024-06-16 23:19:24 +1000

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите слово: ')
+print('Длина слова:', len(my_string))
+print('Верхний регистр', my_string.upper())
+print('Нижний регистр', my_string.lower())
+print('Отсутствуют пробелы', my_string.replace(' ', ''))
+print('Первый символ', my_string[0])
+print('Последний символ', my_string[-1])
